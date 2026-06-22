48e39689b54c0aa5b5e0f38f243f5bcac62d68c1
sumanth balaraju <sumanthrajubalaraju9@gmail.com>
2026-06-22 10:09:28 +0530

    Client.java

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -7,51 +7,75 @@ public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long clientId;
 
-    @Column
-    private String name;
+    @Column(nullable = false)
+    private String firstName;
 
-    @Column
-    private String email;
+    @Column(nullable = false)
+    private String lastName;
 
-    @Column
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
     private String phone;
 
-    public Client() {}
+    @Column(nullable = false)
+    private String email;
 
-    public Client(Long id, String name, String email, String phone) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
+    protected Client() {
+    }
+
+    public Client(String firstName, String lastName, String address, String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
         this.phone = phone;
+        this.email = email;
     }
 
-    public Long getId() {
-        return id;
+    public long getClientId() {
+        return clientId;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getEmail() {
-        return email;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getPhone() {
         return phone;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
     }
 }
